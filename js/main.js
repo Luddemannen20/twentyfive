0fcb68ad338e47f16ce1844aa6df4ce926ba777e
Luddemannen20 <07ludlar@gmail.com>
2025-03-09 12:26:13 +0100

    05/03 - 25

--- js/main.js
+++ js/main.js
@@ -4,9 +4,6 @@ layer.addEventListener("click", showMenu);
 let button = document.querySelector(".menu-button");
 button.addEventListener("click", showMenu)
 
-
-
-
 function showMenu(){
     let menu = document.querySelector("nav.menu");
     menu.classList.toggle("show");
